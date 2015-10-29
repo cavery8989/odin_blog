3f3fe59cee413bb24272e9860761054d27e6f608
Chris J. Avery <c.avery1989@gmail.com>
2015-10-29 13:30:02 +0000

    Added login feature using sorcery gem, added security updates prevening non users changing article

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ end
 resources :tags
 resources :authors
 
+resources :author_sessions, only: [:new, :create, :destroy]
+
+get "login" => 'author_sessions#new'
+get "logout" => 'author_sessions#destroy'
+
 end
